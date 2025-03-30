95fbb1c3498dfa1883b7e20cd58aa57145c7f6aa
Biajacondino <beatriz.jacondino@gmail.com>
2025-03-30 09:47:39 -0300

    Update tcc.py

--- tcc.py
+++ tcc.py
@@ -1,4 +1,7 @@
-<?php
-        echo "Ola Mundo"
-        echo ""
-?>
+import math
+raio = float(input("Entre com o valor do raio:"))
+area = 3.14 * raio * raio
+if area<0:
+    print ("Não é possível calcular")
+else:
+    print ("A area da circunferencia é:",area)
