ebda545c8d189803f9dcd7caa873a9212f17e823
bia jacondino <beatriz.jacondino@gmail.com>
2025-05-13 14:36:12 -0300

    Update speechteste.py

--- speechteste.py
+++ speechteste.py
@@ -5,25 +5,32 @@ def ouvir_microfone():
     microfone = sr.Recognizer()
 
     with sr.Microphone() as source:
-
         microfone.adjust_for_ambient_noise(source)
-
         print("Diga algo: ")
-        
         audio = microfone.listen(source)
 
-    try: 
-        frase = microfone.recognize_google(audio, language= 'pt-BR')
+    try:
+        frase = microfone.recognize_google(audio, language='pt-BR')
         print("Você disse: " + frase)
-        if "navegador" in frase:
-          os.system("start Chrome.exe")
-        if "Github" in frase:
-          os.system("start Github.exe")
+
+        # Executa programas com base na fala
+        if "navegador" in frase.lower():
+            os.system("start chrome")
+        if "github" in frase.lower():
+            os.system("start github")
 
     except sr.UnknownValueError:
-        print ("Não entendi")
+        frase = "Não entendi"
+        print(frase)
+
+    # Escreve a frase (ou "Não entendi") no arquivo .txt
+    with open("meuarquivo.txt", "w", encoding="utf-8") as arquivo:
+        arquivo.write(frase)
+
+    import os
+    print("Salvando arquivo em:", os.getcwd())
 
     return frase
 
-
+# Executa a função
 ouvir_microfone()
